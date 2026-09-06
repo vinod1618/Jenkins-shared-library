3e33bf6a16a78ee94a4216e586be1415e5434233
Vinod <vinodsv1618@gmail.com>
2026-09-07 01:17:50 +0530

    jenkins

--- vars/nodejsEKSPipeline.groovy
+++ vars/nodejsEKSPipeline.groovy
@@ -80,7 +80,7 @@ def call(Map configMap){
                                 script: """
                                     curl -sS \
                                     -H "Accept: application/vnd.github+json" \
-                                    -H "Authorization: Bearer $GITHUB_TOKEN" \
+                                    -H "Authorization: Bearer ${GITHUB_TOKEN}" \
                                     -H "X-GitHub-Api-Version: 2026-03-10" \
                                     "https://api.github.com/repos/vinod1618/${component}-unit-tests/dependabot/alerts?state=open&severity=high,critical&per_page=100"
                                 """,
