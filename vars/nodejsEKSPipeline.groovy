de8ac9147673b14f2bd76c4c76c94419137c1eb7
Vinod <vinodsv1618@gmail.com>
2026-09-07 11:54:57 +0530

    jenkins

--- vars/nodejsEKSPipeline.groovy
+++ vars/nodejsEKSPipeline.groovy
@@ -29,7 +29,7 @@ def call(Map configMap){
                         // Access fields directly
                         appVersion = packageJson.version
                         echo "Building version ${appVersion}"
-                        env
+                        sh 'printenv'
                     }
                 }
             }
